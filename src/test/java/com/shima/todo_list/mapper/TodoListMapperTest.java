9a54b95f5be05d09231d95150940c57293023b8d
”simamumu89” <t.shima89mac.pro@gmail.com>
2024-01-16 22:17:40 +0900

    指定したidでのテスト

--- src/test/java/com/shima/todo_list/mapper/TodoListMapperTest.java
+++ src/test/java/com/shima/todo_list/mapper/TodoListMapperTest.java
@@ -8,10 +8,12 @@ import org.junit.jupiter.api.Test;
 import org.mybatis.spring.boot.test.autoconfigure.MybatisTest;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
+import org.springframework.test.context.jdbc.Sql;
 import org.springframework.transaction.annotation.Transactional;
 
 import java.time.LocalDate;
 import java.util.List;
+import java.util.Optional;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
@@ -23,10 +25,11 @@ class TodoListMapperTest {
     @Autowired
     TodoListMapper todoListMapper;
 
+    //READ機能のDBテスト
     @Test
     @DataSet(value = "datasets/todolists.yml")
     @Transactional
-    void すべてのユーザーが取得できること() {
+    void すべてのタスクが取得できること() {
         List<TodoList> todoLists = todoListMapper.findAll();
         assertThat(todoLists)
                 .hasSize(4)
@@ -36,6 +39,17 @@ class TodoListMapperTest {
                         new TodoList(3, "テスト", LocalDate.of(2023, 12, 8), null, null),
                         new TodoList(4, "リリース", LocalDate.of(2023, 12, 9), null, null)
                 );
+    }
+
+    @Test
+    @Sql(
+            scripts = {"classpath:/sqlannotation/delete-todolists.sql", "classpath:/sqlannotation/insert-todolists.sql"},
+            executionPhase = Sql.ExecutionPhase.BEFORE_TEST_METHOD
+    )
+    @Transactional
+    void 指定したIDのタスク情報を獲得すること() {
+        Optional<TodoList> todoLists = todoListMapper.findById(1);
+        assertThat(todoLists).contains(new TodoList(1, "構想", LocalDate.of(2023, 12, 6), null, null));
     }
 }
 
